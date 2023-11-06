2c473333d4deb277b8bb2a28a671c8ab34d73fdd
ehoffman66 <erik.m.hoffman@gmail.com>
2023-11-06 17:38:53 -0500

    Update Bitcoin component to update ever min

--- src/components/content/BitcoinPriceContent.js
+++ src/components/content/BitcoinPriceContent.js
@@ -1,34 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './BitcoinPriceContent.css';
+import './BitcoinPriceContent.css'; // Make sure this path is correct
 
 const BitcoinPriceContent = () => {
-  const [bitcoinPrice, setBitcoinPrice] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [bitcoinPrice, setBitcoinPrice] = useState(null);
+
+  // Function to fetch Bitcoin price
+  const fetchBitcoinPrice = async () => {
+    try {
+      // Replace with the actual API endpoint you're using to fetch the Bitcoin price
+      const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+      const data = await response.json();
+      setBitcoinPrice(data.bpi.USD.rate_float); // Update state with the new price
+    } catch (error) {
+      console.error('Failed to fetch Bitcoin price:', error);
+    }
+  };
 
   useEffect(() => {
-    const apiEndpoint = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    fetchBitcoinPrice(); // Fetch price on component mount
+    const intervalId = setInterval(fetchBitcoinPrice, 60000); // Fetch price every 60 seconds
 
-    axios.get(apiEndpoint)
-      .then(response => {
-        console.log(response)
-        setBitcoinPrice(response.data.bpi.USD.rate);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('Error fetching Bitcoin data:', error);
-        setError('Failed to load Bitcoin price.');
-        setLoading(false);
-      });
+    return () => clearInterval(intervalId); // Clean up interval on component unmount
   }, []);
 
-  if (loading) return <div>Loading Bitcoin price...</div>;
-  if (error) return <div>{error}</div>;
+  // Format the price as currency
+  const formattedPrice = bitcoinPrice ? bitcoinPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' }) : 'Loading...';
 
   return (
     <div className="bitcoin-price-content">
-      <p>{`$${bitcoinPrice}`}</p>
+      <h3>Bitcoin Price</h3>
+      <p>{formattedPrice}</p>
     </div>
   );
 };
