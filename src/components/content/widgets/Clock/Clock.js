6033476718314ee04f7e7e4921ba6f259d66fbb4
ehoffman66 <erik.m.hoffman@gmail.com>
2023-11-13 22:29:59 -0500

    Remove unneeded comment

--- src/components/content/widgets/Clock/Clock.js
+++ src/components/content/widgets/Clock/Clock.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Clock.css'; // Ensure you have this CSS file
+import './Clock.css';
 
 const ClockContent = () => {
   const [time, setTime] = useState(new Date());
