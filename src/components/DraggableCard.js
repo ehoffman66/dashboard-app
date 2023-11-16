9adce115700e6ed7643e7388b597b94cd7132310
ehoffman66 <erik.m.hoffman@gmail.com>
2023-11-15 22:48:18 -0500

    Full refactor to improce readability and maintainability

--- src/components/DraggableCard.js
+++ src/components/DraggableCard.js
@@ -7,48 +7,35 @@ import ClockContent from './content/widgets/Clock/Clock';
 import BitcoinPriceContent from './content/widgets/BitcoinPrice/BitcoinPrice';
 import BookmarkComponent from './content/widgets/Bookmarks/Bookmarks';
 import F1StandingsWidget from './content/widgets/F1Standings/F1Standings';
-import './DraggableCard.css'; // Ensure this path is correct
+import './DraggableCard.css';
 
-// ... other imports
+const contentComponents = {
+  NFL: NFLContent,
+  NHL: NHLScoresContent,
+  NBA: NBAScoresContent,
+  Clock: ClockContent,
+  Bitcoin: BitcoinPriceContent,
+  Bookmark: BookmarkComponent,
+  F1: F1StandingsWidget,
+};
 
-const DraggableCard = ({ id, title, content, position, onControlledDrag }) => {
-  // Function to handle when dragging stops
+const DraggableCard = ({ id, title, content, position: { x, y }, onControlledDrag }) => {
   const handleStop = (e, data) => {
     onControlledDrag(id, data.x, data.y);
   };
 
-  // Determine the content to render based on the content prop
-  const renderContent = () => {
-    switch (content) {
-      case 'NFL':
-        return <NFLContent />;
-      case 'Clock':
-        return <ClockContent />;
-      case 'Bitcoin':
-        return <BitcoinPriceContent />;
-      case 'NHL':
-        return <NHLScoresContent />;
-      case 'Bookmark':
-        return <BookmarkComponent />;
-      case 'NBA':
-        return <NBAScoresContent />;
-      case 'F1':
-        return <F1StandingsWidget />;
-      default:
-        return <div>No content available</div>;
-    }
-  };
+  const ContentComponent = contentComponents[content] || (() => <div>No content available</div>);
 
   return (
     <Draggable
       handle=".card-title"
-      defaultPosition={{ x: position.x, y: position.y }}
+      defaultPosition={{ x, y }}
       onStop={handleStop}
       bounds="parent"
     >
       <div className="draggable-card">
         <div className="card-title">{title}</div>
-        {renderContent()}
+        <ContentComponent />
       </div>
     </Draggable>
   );
