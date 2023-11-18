102fd44a31a6956850bc0c3945eb1e204954af59
ehoffman66 <erik.m.hoffman@gmail.com>
2023-11-18 14:33:41 -0500

    Major update to NHLScores. Add pagination, fix how socres are show and the current status of the games. Added a function to allow us to just show the period short term

--- src/components/content/widgets/NHLScores/NHLScores.js
+++ src/components/content/widgets/NHLScores/NHLScores.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './NHLScores.css'; // Make sure to import the CSS file
+
+const ITEMS_PER_PAGE = 6;
 
 const NFLScoresContent = () => {
   const [scoreboard, setScoreboard] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const apiEndpoint = 'https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard';
@@ -35,34 +37,59 @@ const NFLScoresContent = () => {
     });
   };
 
+  const getPeriodString = (period) => {
+    switch (period) {
+      case 1: return '1st';
+      case 2: return '2nd';
+      case 3: return '3rd';
+      default: return `${period}th`;
+    }
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
   if (loading) return <div>Loading NFL scores...</div>;
   if (error) return <div>{error}</div>;
+
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const currentPageEvents = scoreboard?.events?.slice(startIndex, endIndex);
 
   return (
     <div className="nfl-scores-content">
       <div className="games-grid">
-        {scoreboard?.events?.map((event, index) => {
+        {currentPageEvents?.map((event, index) => {
           const isScheduled = event.status.type.description === "Scheduled";
           const sortedCompetitors = event.competitions[0]?.competitors?.sort((a, b) => a.homeAway === 'home' ? 1 : -1);
           return (
             <div key={index} className="game">
-              <div className="game-status">
-                {isScheduled ? formatDate(event.date) : event.status.type.description}
-              </div>
+            <div className="game-status">
+              {isScheduled ? formatDate(event.date) : event.status.type.description === "Final" ? "Final" : `${event.status.type.description} - ${getPeriodString(event.status.period)} (${event.status.displayClock})`}
+            </div>
               {sortedCompetitors?.map((team) => (
                 <div key={team.id} className="team">
                   {team.team.logo && (
                     <img src={team.team.logo} alt={`${team.team.displayName} Logo`} className="team-logo" />
                   )}
-                  <span>{team.team.displayName} - {team.score || 'TBD'}</span>
+                    <span>{team.team.displayName} {isScheduled ? '' : '- ' + team.score}</span>
                 </div>
               ))}
             </div>
           );
         })}
       </div>
+      <div className="pagination">
+        {Array(Math.ceil((scoreboard?.events?.length || 0) / ITEMS_PER_PAGE)).fill().map((_, index) => (
+          <button key={index} onClick={() => handlePageChange(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
-};
-
-export default NFLScoresContent;
+  
+  };
+  
+  export default NHLScoresContent;
