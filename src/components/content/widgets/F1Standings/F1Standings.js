76912a69af6c81e00e6ffe85154b1141d9063871
ehoffman66 <erik.m.hoffman@gmail.com>
2023-11-27 00:07:54 -0500

    Update styline for F1

--- src/components/content/widgets/F1Standings/F1Standings.js
+++ src/components/content/widgets/F1Standings/F1Standings.js
@@ -1,41 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './F1Standings.css';
 
 const ITEMS_PER_PAGE = 10;
 
 const F1StandingsWidget = () => {
   const [standings, setStandings] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [standingsType, setStandingsType] = useState('driver'); // 'driver' or 'constructor'
 
   useEffect(() => {
-    const endpoint = `https://ergast.com/api/f1/current/${standingsType}Standings.json`;
-    
-    axios.get(endpoint)
+    fetch(`https://ergast.com/api/f1/current/${standingsType}Standings.json`)
       .then(response => {
-        const standingsList = standingsType === 'driver'
-          ? response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings
-          : response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
-        setStandings(standingsList);
-        setLoading(false);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
       })
-      .catch(err => {
-        const errMsg = `Failed to load F1 ${standingsType.charAt(0).toUpperCase() + standingsType.slice(1)} Standings.`;
-        console.error(`${errMsg}`, err);
-        setError(errMsg);
-        setLoading(false);
-      });
+      .then(data => {
+        const standingsList = data.MRData.StandingsTable.StandingsLists[0];
+        setStandings(standingsType === 'driver' ? standingsList.DriverStandings : standingsList.ConstructorStandings);
+      })
+      .catch(error => console.log('There was a problem with the fetch operation: ' + error.message));
   }, [standingsType]);
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
-
-  if (loading) return <div>Loading F1 Standings...</div>;
-  if (error) return <div>Error: {error}</div>;
 
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
@@ -44,10 +34,20 @@ const F1StandingsWidget = () => {
   return (
     <div className="f1-standings-widget">
       <div style={{ display: 'flex', marginBottom: '10px' }}>
-        <div className="standings-type" onClick={() => setStandingsType('driver')}>Driver</div>
-        <div className="standings-type" onClick={() => setStandingsType('constructor')}>Constructor</div>
+        <div 
+          className={`standings-type ${standingsType === 'driver' ? 'underline' : ''}`} 
+          onClick={() => setStandingsType('driver')}
+        >
+          Driver
+        </div>
+        <div 
+          className={`standings-type ${standingsType === 'constructor' ? 'underline' : ''}`} 
+          onClick={() => setStandingsType('constructor')}
+        >
+          Constructor
+        </div>
       </div>
-      <ol>
+      <ul>
         {currentPageStandings.map((standing, index) => (
           <li key={index}>
             <span className="driver-name">
@@ -60,7 +60,7 @@ const F1StandingsWidget = () => {
             </span>
           </li>
         ))}
-      </ol>
+      </ul>
       <div className="pagination">
         {Array(Math.ceil(standings.length / ITEMS_PER_PAGE)).fill().map((_, index) => (
           <button className="pagination-button" key={index} onClick={() => handlePageChange(index + 1)}>
