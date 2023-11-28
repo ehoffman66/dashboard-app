b136453cadf05f6b1482e9c8c1e1b9240aa4895d
ehoffman66 <erik.m.hoffman@gmail.com>
2023-11-27 22:31:56 -0500

    Update CollegeFootball.js to show rankings. Will need to be styled later

--- src/components/content/widgets/CollegeFootball/CollegeFootballScores.js
+++ src/components/content/widgets/CollegeFootball/CollegeFootballScores.js
@@ -5,9 +5,11 @@ const ITEMS_PER_PAGE = 6;
 
 const CollegeFootballScores = () => {
   const [scores, setScores] = useState([]);
+  const [rankings, setRankings] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
+  const [view, setView] = useState('scores'); // 'scores' or 'rankings'
 
   useEffect(() => {
     const fetchScores = async () => {
@@ -21,42 +23,92 @@ const CollegeFootballScores = () => {
       }
     };
 
-    fetchScores();
-  }, []);
+    const fetchRankings = async () => {
+      try {
+        const response = await axios.get('https://site.api.espn.com/apis/site/v2/sports/football/college-football/rankings');
+        setRankings(response.data.rankings[0]['ranks'].slice(0, 25));
+        setLoading(false);
+        console.log(response.data.rankings[0]['ranks'].slice(0, 25));
+      } catch (error) {
+        setError('Failed to load College Football rankings.');
+        setLoading(false);
+      }
+    };
+
+    if (view === 'scores') {
+      fetchScores();
+    } else if (view === 'rankings') {
+      fetchRankings();
+    }
+  }, [view]);
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-  const currentPageScores = scores.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE);
+  const currentPageData = view === 'scores' ? scores.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE) : rankings;
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
   return (
     <div className="college-football-scores-content">
+      <div style={{ display: 'flex', marginBottom: '10px' }}>
+        <div 
+          className={`view-type ${view === 'scores' ? 'underline' : ''}`} 
+          onClick={() => setView('scores')}
+        >
+          Scores
+        </div>
+        <div 
+          className={`view-type ${view === 'rankings' ? 'underline' : ''}`} 
+          onClick={() => setView('rankings')}
+        >
+          Rankings
+        </div>
+      </div>
       <div className="games-grid">
-        {currentPageScores.map((game, index) => {
-          const isCompleted = game.status.type.completed;
-          const sortedCompetitors = game.competitions[0]?.competitors?.sort((a, b) => a.homeAway === 'home' ? 1 : -1);
-          return (
-            <div key={index} className="game">
-              <div className="game-status">
-                {isCompleted ? 'Final' : game.status.type.detail}
-              </div>
-              {sortedCompetitors?.map((team) => (
-                <div key={team.id} className="team">
+        {view === 'scores' ? (
+          currentPageData.map((game, index) => {
+            const isCompleted = game.status?.type?.completed;
+            const sortedCompetitors = game.competitions?.[0]?.competitors?.sort((a, b) => a.homeAway === 'home' ? 1 : -1);
+            return (
+              <div key={index} className="game">
+                <div className="game-status">
+                  {isCompleted ? 'Final' : game.status?.type?.detail}
+                </div>
+                {sortedCompetitors?.map((team) => (
+                  <div key={team.id} className="team">
                     {team.team.logo && (
-                    <img src={team.team.logo} alt={`${team.team.name} Logo`} className="team-logo" />
+                      <img src={team.team.logo} alt={`${team.team.name} Logo`} className="team-logo" />
                     )}
-                    <span>{team.team.name} {isCompleted ? `- ${team.score}` : ''}</span>                </div>
+                    <span>{team.team.name} {isCompleted ? `- ${team.score}` : ''}</span>
+                  </div>
                 ))}
+              </div>
+            );
+          })
+        ) : (
+          <>
+            <div className="rankings-container">
+              {rankings.slice(0, 13).map((ranking, index) => (
+                <div key={index} className="ranking">
+                  <span>{ranking.current} {ranking.team.name}</span>
+                </div>
+              ))}
             </div>
-          );
-        })}
+            <div className="rankings-container">
+              {rankings.slice(13).map((ranking, index) => (
+                <div key={index} className="ranking">
+                  <span>{ranking.current} {ranking.team.name}</span>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
       <div className="pagination">
-        {Array(Math.ceil(scores.length / ITEMS_PER_PAGE)).fill().map((_, index) => (
+        {Array(Math.ceil((view === 'scores' ? scores.length : rankings.length) / ITEMS_PER_PAGE)).fill().map((_, index) => (
           <button key={index} onClick={() => handlePageChange(index + 1)}>
             {index + 1}
           </button>
