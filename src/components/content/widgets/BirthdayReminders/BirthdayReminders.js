753c97fb43358b0795822ab07df0691318b1d95f
ehoffman66 <erik.m.hoffman@gmail.com>
2023-11-29 23:52:26 -0500

    Major updates to the styling for Birthday Reminders and added delete button

--- src/components/content/widgets/BirthdayReminders/BirthdayReminders.js
+++ src/components/content/widgets/BirthdayReminders/BirthdayReminders.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import './BirthdayReminders.css';
 
 const BirthdayWidget = () => {
   const [birthdays, setBirthdays] = useState([]);
-  const [newBirthday, setNewBirthday] = useState({ name: '', date: '' });
+  const [newBirthday, setNewBirthday] = useState({ name: '', month: '', day: '' });
 
   useEffect(() => {
-    // Load birthdays from local storage
     const loadedBirthdays = JSON.parse(localStorage.getItem('birthdays')) || [];
     setBirthdays(loadedBirthdays);
   }, []);
@@ -14,7 +14,13 @@ const BirthdayWidget = () => {
     const updatedBirthdays = [...birthdays, newBirthday];
     setBirthdays(updatedBirthdays);
     localStorage.setItem('birthdays', JSON.stringify(updatedBirthdays));
-    setNewBirthday({ name: '', date: '' }); // Reset input fields
+    setNewBirthday({ name: '', month: '', day: '' });
+  };
+
+  const deleteBirthday = (index) => {
+    const updatedBirthdays = birthdays.filter((_, i) => i !== index);
+    setBirthdays(updatedBirthdays);
+    localStorage.setItem('birthdays', JSON.stringify(updatedBirthdays));
   };
 
   const handleInputChange = (e) => {
@@ -23,12 +29,6 @@ const BirthdayWidget = () => {
 
   return (
     <div className="birthday-widget">
-      <h2>Birthdays</h2>
-      <div>
-        {birthdays.map((birthday, index) => (
-          <div key={index}>{birthday.name} - {birthday.date}</div>
-        ))}
-      </div>
       <div>
         <input 
           type="text" 
@@ -38,12 +38,30 @@ const BirthdayWidget = () => {
           placeholder="Name" 
         />
         <input 
-          type="date" 
-          name="date" 
-          value={newBirthday.date} 
+          type="text" 
+          name="month" 
+          value={newBirthday.month} 
           onChange={handleInputChange} 
+          placeholder="Month" 
+          maxLength="2"
         />
-        <button onClick={addBirthday}>Add Birthday</button>
+        <input 
+          type="text" 
+          name="day" 
+          value={newBirthday.day} 
+          onChange={handleInputChange} 
+          placeholder="Day" 
+          maxLength="2"
+        />
+        <button onClick={addBirthday}>Add</button>
+      </div>
+      <div className="birthday-list">
+        {birthdays.map((birthday, index) => (
+          <div key={index} className="birthday">
+            {birthday.name} - {birthday.month}/{birthday.day}
+            <button onClick={() => deleteBirthday(index)}>Delete</button>
+          </div>
+        ))}
       </div>
     </div>
   );
