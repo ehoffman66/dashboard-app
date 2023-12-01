bcd33f0998c2febb11dbb026e5d9489370184a09
ehoffman66 <erik.m.hoffman@gmail.com>
2023-11-30 22:42:31 -0500

    Update Dashboard to allow for a bit more responsiveness when browser is resized

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,16 +4,16 @@ import Navbar from './Navbar';
 import './Dashboard.css';
 
 const DEFAULT_CARDS = [
-  { id: 1, title: 'NFL Scores', content: 'NFL', position: { x: -171, y: 0 }},
-  { id: 2, title: 'Current Time', content: 'Clock', position: { x: -167, y: 0 }},
-  { id: 3, title: 'Bitcoin Price', content: 'Bitcoin', position: { x: -128, y: 0 }},
-  { id: 4, title: 'NHL Scores', content: 'NHL', position: { x: -35, y: -4 }},
-  { id: 5, title: 'Bookmarks', content: 'Bookmark', position: { x: 24, y: -483 }},
-  { id: 6, title: 'NBA Scores', content: 'NBA', position: { x: -307, y: -266 }},
-  { id: 7, title: 'F1 Driver Standings', content: 'F1', position: { x: 641, y: 300 }},
-  { id: 8, title: 'College Football Scores', content: 'College', position: { x: 641, y: 200 }},
-  { id: 9, title: 'NASA Daily Photo', content: 'NASA', position: { x: 741, y: 200 }},
-  { id: 10, title: 'Birthday Reminders', content: 'Birthday', position: { x: 741, y: 200 }},
+  { id: 1, title: 'NFL Scores', content: 'NFL', position: { x: 100, y: 100 }},
+  { id: 2, title: 'Current Time', content: 'Clock', position: { x: 200, y: 200 }},
+  { id: 3, title: 'Bitcoin Price', content: 'Bitcoin', position: { x: 300, y: 300 }},
+  { id: 4, title: 'NHL Scores', content: 'NHL', position: { x: 400, y: 400 }},
+  { id: 5, title: 'Bookmarks', content: 'Bookmark', position: { x: 500, y: 500 }},
+  { id: 6, title: 'NBA Scores', content: 'NBA', position: { x: 600, y: 600 }},
+  { id: 7, title: 'F1 Driver Standings', content: 'F1', position: { x: 700, y: 700 }},
+  { id: 8, title: 'College Football Scores', content: 'College', position: { x: 800, y: 800 }},
+  { id: 9, title: 'NASA Daily Photo', content: 'NASA', position: { x: 900, y: 900 }},
+  { id: 10, title: 'Birthday Reminders', content: 'Birthday', position: { x: 1000, y: 1000 }},
 ];
 
 const Dashboard = () => {
@@ -32,7 +32,7 @@ const Dashboard = () => {
   const onControlledDrag = useCallback((id, x, y) => {
     const newCards = cards.map((card) => {
       if (card.id === id) {
-        return { ...card, position: { x, y }};
+        return { ...card, position: { x: x, y: y }};
       }
       return card;
     });
@@ -48,13 +48,12 @@ const Dashboard = () => {
 
   return (
     <div>
-      <Navbar widgets={DEFAULT_CARDS} /> {/* Pass DEFAULT_CARDS as widgets prop */}
+      <Navbar widgets={cards} />
       <div className="dashboard">
-        {cards.map((card, index) => (
+        {cards.map((card) => (
           <DraggableCard
             key={card.id}
             id={card.id}
-            index={index}
             title={card.title}
             content={card.content}
             position={card.position}
@@ -65,6 +64,5 @@ const Dashboard = () => {
       </div>
     </div>
   );
-};
-
+};    
 export default Dashboard;
