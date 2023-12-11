7cc66eebfc3fe7023b6a98a87125e30800d7575d
ehoffman66 <erik.m.hoffman@gmail.com>
2023-12-10 21:41:32 -0500

    Make sure styling is added to widget and update the styling a bit

--- src/components/content/widgets/HackerNews/HackerNews.js
+++ src/components/content/widgets/HackerNews/HackerNews.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './HackerNews.css';
 
 const HackerNewsWidget = () => {
   const [topStories, setTopStories] = useState([]);
