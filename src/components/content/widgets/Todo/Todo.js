74e3bc9ffdaeda904e10f2062a77ab6504e36857
ehoffman66 <erik.m.hoffman@gmail.com>
2023-12-12 23:09:02 -0500

    New widget! This one is for users to create todo lists

---
+++ src/components/content/widgets/Todo/Todo.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const TodoWidget = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { id: Date.now(), text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  };
+
+  const toggleTaskCompletion = (taskId) => {
+    setTasks(tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    }));
+  };
+
+  return (
+    <div className="todo-widget">
+      <h2>To-Do List</h2>
+      <input 
+        type="text" 
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Add new task"
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id} style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+            {task.text}
+            <button onClick={() => toggleTaskCompletion(task.id)}>
+              {task.completed ? 'Unmark' : 'Complete'}
+            </button>
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoWidget;
