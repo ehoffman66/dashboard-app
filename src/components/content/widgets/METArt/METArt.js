730c33aa0606914c696913cd916d287653b9b6a4
ehoffman66 <erik.m.hoffman@gmail.com>
2023-12-24 16:55:04 -0500

    Remove unneeded comments and console

--- src/components/content/widgets/METArt/METArt.js
+++ src/components/content/widgets/METArt/METArt.js
@@ -3,25 +3,20 @@ import axios from 'axios';
 import './METArt.css';
 
 const ArtworkWidget = () => {
-  // State variables for the artwork data and loading status
   const [artwork, setArtwork] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchArtwork = async () => {
       try {
-        // Fetch the list of object IDs for a specific department
-        const departmentId = 19; // Example: Assuming the department ID for photographs is 19 (you need to verify the actual ID)
+        const departmentId = 19;
         const response = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${departmentId}`);
         const objectIds = response.data.objectIDs;
 
-        // Select a random object ID from the list
         const randomObjectId = objectIds[Math.floor(Math.random() * objectIds.length)];
 
-        // Fetch the details for the selected object
         const objectDetailsResponse = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomObjectId}`);
         setArtwork(objectDetailsResponse.data);
-        console.log(objectDetailsResponse.data);
       } catch (error) {
         console.error('Error fetching artwork data:', error);
       } finally {
@@ -29,14 +24,11 @@ const ArtworkWidget = () => {
       }
     };
 
-    // Fetch the artwork data when the component mounts
     fetchArtwork();
   }, []);
 
-  // Render a loading message while the data is being fetched
   if (loading) return <div>Loading artwork...</div>;
 
-  // Render the artwork data
   return (
     <div className="artwork-widget">
       <h2>Photograph of the Day</h2>
