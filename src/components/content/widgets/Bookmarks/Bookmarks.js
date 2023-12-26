89e7ae1b37766a515af26554df0d58d4cc9fec51
ehoffman66 <erik.m.hoffman@gmail.com>
2023-12-25 19:46:00 -0500

    Remove unneeded comments

--- src/components/content/widgets/Bookmarks/Bookmarks.js
+++ src/components/content/widgets/Bookmarks/Bookmarks.js
@@ -7,54 +7,49 @@ const BookmarkComponent = () => {
   const [newBookmark, setNewBookmark] = useState('');
 
   useEffect(() => {
-    // Load bookmarks from local storage on initial render
     const savedBookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
     setBookmarks(savedBookmarks);
   }, []);
 
   const addBookmark = (event) => {
     event.preventDefault();
-    if (!newBookmark) return; // Don't add empty bookmarks
+    if (!newBookmark) return;
 
-    // Prepend https:// if no protocol is specified
     const protocolPattern = /^(http:\/\/|https:\/\/)/i;
     const bookmarkUrl = protocolPattern.test(newBookmark)
       ? newBookmark
       : `https://${newBookmark}`;
 
-    if (bookmarks.includes(bookmarkUrl)) return; // Don't add duplicate bookmarks
+    if (bookmarks.includes(bookmarkUrl)) return;
 
     const updatedBookmarks = [...bookmarks, bookmarkUrl];
     setBookmarks(updatedBookmarks);
-    setNewBookmark(''); // Clear the input field after adding
-    localStorage.setItem('bookmarks', JSON.stringify(updatedBookmarks)); // Update local storage
+    setNewBookmark('');
+    localStorage.setItem('bookmarks', JSON.stringify(updatedBookmarks));
   };
 
   const removeBookmark = (index) => {
     const updatedBookmarks = bookmarks.filter((_, i) => i !== index);
     setBookmarks(updatedBookmarks);
-    localStorage.setItem('bookmarks', JSON.stringify(updatedBookmarks)); // Update local storage
+    localStorage.setItem('bookmarks', JSON.stringify(updatedBookmarks));
   };
 
   const handleInputChange = (event) => {
     setNewBookmark(event.target.value);
   };
 
-  // Utility function to capitalize the first letter of a string
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  // Extract the domain name for display and capitalize the first letter
   const formatDisplayUrl = (url) => {
     try {
       let domain = new URL(url).hostname;
-      // Remove 'www.' if present, capitalize the first letter of the domain
       domain = domain.replace(/^www\./i, '');
       return capitalizeFirstLetter(domain);
     } catch (error) {
       console.error('Error formatting URL:', error);
-      return url; // Fallback to the full URL in case of an error
+      return url;
     }
   };
 
